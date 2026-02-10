1a8b35ffe6db42239836df365bbeee4dbbfda889
ZackDevsCrud <officialzackattack15@gmail.com>
2026-02-10 18:23:34 -0500

    backend checks

--- CDInterestCalculator/resources/scripts/calculate.js
+++ CDInterestCalculator/resources/scripts/calculate.js
@@ -1,7 +1,33 @@
 const calculateButton = document.getElementsByClassName('calculate')[0];
 
+const cdLengthInput = document.getElementsByName('cdLength')[0]
+const interestRateInput = document.getElementsByName('interestRate')[0]
+const initialDepositInput = document.getElementsByName('initialDeposit')[0]
+const renewalCountInput = document.getElementsByName('renewalCount')[0]
+const renewalDepositInput = document.getElementsByName('renewalDeposit')[0]
+
 function roundToHundredth(number) {
     return Math.round(number * 100) / 100;
+}
+
+function checkInputs() {
+    let cdLength = Math.floor(Number(cdLengthInput.value));
+    let interestRate = (Number(interestRateInput.value));
+    let initialDeposit = Math.floor(Number(initialDepositInput.value));
+    let renewalCount = Math.floor(Number(renewalCountInput.value));
+    let renewalDeposit = Math.floor(Number(renewalDepositInput.value));
+
+    if (cdLength < 1) cdLength = 1;
+    if (interestRate < 0.1) interestRate = 0.1;
+    if (initialDeposit < 1) initialDeposit = 1;
+    if (renewalCount < 0) renewalCount = 0;
+    if (renewalDeposit < 0) renewalDeposit = 0;
+
+    cdLengthInput.value = cdLength;
+    initialDepositInput.value = initialDeposit;
+    interestRateInput.value = interestRate;
+    renewalCountInput.value = renewalCount;
+    renewalDepositInput.value = renewalDeposit;
 }
 
 function calculateData() {
@@ -39,3 +65,8 @@ function calculateData() {
 calculateData();
 
 calculateButton.addEventListener('click', calculateData)
+cdLengthInput.addEventListener('focusout', checkInputs)
+interestRateInput.addEventListener('focusout', checkInputs)
+initialDepositInput.addEventListener('focusout', checkInputs)
+renewalCountInput.addEventListener('focusout', checkInputs)
+renewalDepositInput.addEventListener('focusout', checkInputs)
